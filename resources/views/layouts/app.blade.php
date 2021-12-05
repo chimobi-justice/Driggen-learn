42a52533dc90ee02d4f1cf02e68bdf8f879bd32c
chimobi-justice <chimobi.justice@gmail.com>
2021-12-05 19:30:09 +0100

    add image-favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,10 @@
   <link rel="stylesheet" href="{{ asset('css/contact.css') }}">
   <link rel="stylesheet" href="{{ asset('css/Details.css') }}">
   <link rel="stylesheet" href="{{ asset('css/Footer.css') }}">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
   <title>{{ config('app.name')}}</title>
 </head>
 <body class="bg-white">
--- resources/views/layouts/dashboard/users/app.blade.php
+++ resources/views/layouts/dashboard/users/app.blade.php
@@ -15,6 +15,10 @@
   <link rel="stylesheet" href="{{ asset('css/Discover.css') }}">
   <link rel="stylesheet" href="{{ asset('css/account.css') }}">
   <link rel="stylesheet" href="{{ asset('css/settings.css') }}">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
   <title>{{ config('app.name')}}</title>
 </head>
 <body class="bg-white">
